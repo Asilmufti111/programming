229db6b1162724bdf8f810d3eda2c27299ecbc2f
Asilmufti111 <126459608+Asilmufti111@users.noreply.github.com>
2023-09-30 15:55:14 +0300

    Delete Labs directory

--- Labs/src/Lab10/Car.java
+++
@@ -1,9 +0,0 @@
-package Lab10;
-
-public class Car extends basicVehicle {
-
-    public Car(int noOfWheels, int noOfSeats, int noOfDoors, String type) {
-        super(noOfWheels, noOfSeats, noOfDoors, type);
-    }
-
-}
